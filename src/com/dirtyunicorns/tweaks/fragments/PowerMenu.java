fbb59535545563abae4958207615e845036f697d
Alex Cruz <alex@dirtyunicorns.com>
2018-11-24 02:01:32 -0600

    DUTweaks: Hide lockscreen visibility switch conditionally
    Since this switch is only to hide the power menu while the lockscreen
    is secured, it makes sense to only have it visible while the device
    is secured. We have many other options both custom and AOSP that work
    like this, so is only fitting that we continue this with this switch.
    
    Change-Id: I05a1059c63dcd21b239beccf167cd9b6872e9314

--- src/com/dirtyunicorns/tweaks/fragments/PowerMenu.java
+++ src/com/dirtyunicorns/tweaks/fragments/PowerMenu.java
@@ -42,9 +42,11 @@ public class PowerMenu extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener, Indexable {
 
     private static final String KEY_LOCKDOWN_IN_POWER_MENU = "lockdown_in_power_menu";
+
     private static final int MY_USER_ID = UserHandle.myUserId();
 
     private SwitchPreference mPowerMenuLockDown;
+    private PreferenceCategory mAdvancedCategory;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -55,12 +57,15 @@ public class PowerMenu extends SettingsPreferenceFragment
         final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
 
         mPowerMenuLockDown = (SwitchPreference) findPreference(KEY_LOCKDOWN_IN_POWER_MENU);
+        mAdvancedCategory = (PreferenceCategory) findPreference("powermenu_advanced_category");
+
         if (lockPatternUtils.isSecure(MY_USER_ID)) {
             mPowerMenuLockDown.setChecked((Settings.Secure.getInt(getContentResolver(),
                     Settings.Secure.LOCKDOWN_IN_POWER_MENU, 0) == 1));
             mPowerMenuLockDown.setOnPreferenceChangeListener(this);
         } else {
             prefSet.removePreference(mPowerMenuLockDown);
+            prefSet.removePreference(mAdvancedCategory);
         }
     }
 
