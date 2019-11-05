15257c2230a637cba6423ec88f320ae70073b8bb
LorDClockaN <davor@losinj.com>
2019-11-05 23:40:26 +0700

    DUTweaks: Powermenu Torch option [2/2]
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- src/com/dirtyunicorns/tweaks/fragments/PowerMenu.java
+++ src/com/dirtyunicorns/tweaks/fragments/PowerMenu.java
@@ -16,6 +16,7 @@
 
 package com.dirtyunicorns.tweaks.fragments;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
@@ -31,8 +32,7 @@ import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-
+import com.android.internal.util.du.Utils;
 import com.android.internal.widget.LockPatternUtils;
 
 import java.util.ArrayList;
@@ -43,9 +43,11 @@ public class PowerMenu extends SettingsPreferenceFragment
 
     private static final String KEY_LOCKDOWN_IN_POWER_MENU = "lockdown_in_power_menu";
     private static final String POWER_MENU_ANIMATIONS = "power_menu_animations";
+    private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
 
     private static final int MY_USER_ID = UserHandle.myUserId();
 
+    private SwitchPreference mPowermenuTorch;
     private SwitchPreference mPowerMenuLockDown;
     private PreferenceCategory mAdvancedCategory;
     private ListPreference mPowerMenuAnimations;
@@ -57,6 +59,8 @@ public class PowerMenu extends SettingsPreferenceFragment
 
         final PreferenceScreen prefSet = getPreferenceScreen();
         final LockPatternUtils lockPatternUtils = new LockPatternUtils(getActivity());
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
 
         mPowerMenuLockDown = (SwitchPreference) findPreference(KEY_LOCKDOWN_IN_POWER_MENU);
         mAdvancedCategory = (PreferenceCategory) findPreference("powermenu_advanced_category");
@@ -75,6 +79,15 @@ public class PowerMenu extends SettingsPreferenceFragment
                 getContentResolver(), Settings.System.POWER_MENU_ANIMATIONS, 0)));
         mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
         mPowerMenuAnimations.setOnPreferenceChangeListener(this);
+
+        mPowermenuTorch = (SwitchPreference) findPreference(KEY_POWERMENU_TORCH);
+        mPowermenuTorch.setOnPreferenceChangeListener(this);
+        if (!Utils.deviceHasFlashlight(getActivity())) {
+            prefScreen.removePreference(mPowermenuTorch);
+        } else {
+        mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
+                Settings.System.POWERMENU_TORCH, 0) == 1));
+        }
     }
 
     @Override
@@ -89,6 +102,11 @@ public class PowerMenu extends SettingsPreferenceFragment
                     Integer.valueOf((String) objValue));
             mPowerMenuAnimations.setValue(String.valueOf(objValue));
             mPowerMenuAnimations.setSummary(mPowerMenuAnimations.getEntry());
+            return true;
+        } else if (preference == mPowermenuTorch) {
+            boolean value = (Boolean) objValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_TORCH, value ? 1 : 0);
             return true;
         }
         return false;
