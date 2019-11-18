53fa14fa2632821ca6d6887900661d93ffa214c8
AnkitGourav <Ankitgourav035@gmail.com>
2019-11-18 16:01:38 +0530

    DUTweaks: NetworkTraffic: Add Combined & Dynamic Mode [2/2]
    spezi77 edits:
    -Squashed into one commit
    -Updated strings
    
    Change-Id: I291c802c0d4b252e5205aec58bb0db465eb2ca3c
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- src/com/dirtyunicorns/tweaks/fragments/TrafficIndicators.java
+++ src/com/dirtyunicorns/tweaks/fragments/TrafficIndicators.java
@@ -44,6 +44,7 @@ import android.text.TextUtils;
 import android.view.View;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
