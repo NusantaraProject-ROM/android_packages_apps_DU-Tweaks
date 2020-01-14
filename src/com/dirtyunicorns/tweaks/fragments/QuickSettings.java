43394a8be99f20330bad20559b005250d84557ce
Genkzsz11 <Genkzsz11@gmail.com>
2020-01-14 18:43:10 +0700

    Revert "DUTweaks: QS themes [2/2]"
    This reverts commit 9439addd5cb25c2bc89aa0f08d543d12457a1ef7.

--- src/com/dirtyunicorns/tweaks/fragments/QuickSettings.java
+++ src/com/dirtyunicorns/tweaks/fragments/QuickSettings.java
@@ -42,7 +42,6 @@ import java.util.List;
 import com.dirtyunicorns.support.preferences.CustomSeekBarPreference;
 import com.dirtyunicorns.support.preferences.SystemSettingMasterSwitchPreference;
 import com.dirtyunicorns.support.preferences.SystemSettingEditTextPreference;
-import com.dirtyunicorns.support.colorpicker.ColorPickerPreference;
 
 public class QuickSettings extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener, Indexable {
@@ -50,9 +49,7 @@ public class QuickSettings extends SettingsPreferenceFragment
 
     private static final String QUICK_PULLDOWN = "quick_pulldown";
     private static final String STATUS_BAR_CUSTOM_HEADER = "status_bar_custom_header";
-    private static final String QS_PANEL_COLOR = "qs_panel_color";
     private static final String FOOTER_TEXT_STRING = "footer_text_string";
-    static final int DEFAULT_QS_PANEL_COLOR = 0xffffffff;
 
     private ListPreference mQuickPulldown;
     private CustomSeekBarPreference mQsRowsPort;
@@ -60,7 +57,6 @@ public class QuickSettings extends SettingsPreferenceFragment
     private CustomSeekBarPreference mQsColumnsPort;
     private CustomSeekBarPreference mQsColumnsLand;
     private SystemSettingMasterSwitchPreference mCustomHeader;
-    private ColorPickerPreference mQsPanelColor;
     private SystemSettingEditTextPreference mFooterString;
 
     @Override
@@ -106,14 +102,6 @@ public class QuickSettings extends SettingsPreferenceFragment
                 Settings.System.STATUS_BAR_CUSTOM_HEADER, 0);
         mCustomHeader.setChecked(qsHeader != 0);
         mCustomHeader.setOnPreferenceChangeListener(this);
-
-        mQsPanelColor = (ColorPickerPreference) findPreference(QS_PANEL_COLOR);
-        mQsPanelColor.setOnPreferenceChangeListener(this);
-        int intColor = Settings.System.getIntForUser(getContentResolver(),
-                Settings.System.QS_PANEL_BG_COLOR, DEFAULT_QS_PANEL_COLOR, UserHandle.USER_CURRENT);
-        String hexColor = String.format("#%08x", (0xffffffff & intColor));
-        mQsPanelColor.setSummary(hexColor);
-        mQsPanelColor.setNewPreviewColor(intColor);
 
         mFooterString = (SystemSettingEditTextPreference) findPreference(FOOTER_TEXT_STRING);
         mFooterString.setOnPreferenceChangeListener(this);
@@ -161,14 +149,6 @@ public class QuickSettings extends SettingsPreferenceFragment
             boolean header = (Boolean) newValue;
             Settings.System.putInt(resolver,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER, header ? 1 : 0);
-            return true;
-        } else if (preference == mQsPanelColor) {
-            String hex = ColorPickerPreference.convertToARGB(
-                    Integer.valueOf(String.valueOf(newValue)));
-            preference.setSummary(hex);
-            int intHex = ColorPickerPreference.convertToColorInt(hex);
-            Settings.System.putIntForUser(getContentResolver(),
-                    Settings.System.QS_PANEL_BG_COLOR, intHex, UserHandle.USER_CURRENT);
             return true;
         } else if (preference == mFooterString) {
             String value = (String) newValue;
