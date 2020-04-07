4d0b8b98935a33ce6a22970cc10e4f9269284d58
Genkzsz11 <Genkzsz11@gmail.com>
2020-04-07 22:30:00 +0700

    DUTweaks: fix derp

--- src/com/dirtyunicorns/tweaks/fragments/GamingModeReceiver.java
+++ src/com/dirtyunicorns/tweaks/fragments/GamingModeReceiver.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.diryunicorns.tweaks.fragments;
+package com.dirtyunicorns.tweaks.fragments;
 
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
